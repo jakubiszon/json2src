58ff963387f3d0f64562a6c66035d654fba339df
jakubiszon <jakubzurawski@gmail.com>
2021-10-31 13:59:42 +0100

    update code comments

--- index.js
+++ index.js
@@ -7,24 +7,38 @@ const engineRunner = require('./src/engine-runner');
  * @param {string} [partialsRoot] directory storing the partials, subdirectories are included
  * @param {Object.<string, function>} [helpers] an object mapping the helper functions
  */
+function EngineParameters() {
+	this.templateRoot = "";
+	this.partialsRoot = "";
+	this.helpers = {};
+}
 
 /**
- * @function fileNameMaker returns filenames (without paths) to use when saving output files
+ * @function fileNameFunc returns filenames (without paths) to use when saving output files
  * @param {string} templateName path and anme of the template, relative to the templateRoot of the engine
  * @returns {string} filename (without path) to use to save the output file
  */
+function fileNameFunc( templateName ) {
+	return "filename";
+}
 
  /**
   * @typedef RunParameters
   * @property {Object} data - data passed to each of the layouts specified in the engine
   * @property {string} outputRoot - folder to which compiled output files will be saved
-  * @property {string|fileNameMaker} filePrefix - NOT USED YET prefix to use for each filename or a function to make the filename
+  * @property {string|fileNameFunc} filePrefix - NOT USED YET prefix to use for each filename or a function to make the filename
   */
+function RunParameters () {
+	this.data = {};
+	this.outputRoot = "";
+	this.filePrefix = "";
+	this.consoleOutput = true;
+}
 
 /**
  * Returns an engine function.
  * @function
- * @param {EngineParameters} engineParameters test test test
+ * @param {EngineParameters} engineParameters
  */
 module.exports = async function ({ templateRoot, partialsRoot, helpers }) {
 
@@ -38,7 +52,7 @@ module.exports = async function ({ templateRoot, partialsRoot, helpers }) {
 	/**
 	 * Loops the data through all templates defined in the engine and saves them into outputRoot folder
 	 * @function
-	 * @param {RunParameters} runParameters test commens
+	 * @param {RunParameters} runParameters 
 	 * @returns {Promise}
 	 */
 	function engineRun({ data, outputRoot, filePrefix, consoleOutput }) {
